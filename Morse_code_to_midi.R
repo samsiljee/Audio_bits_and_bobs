efcf39119044cdc3ad6388754b929ae60d30f977
samsiljee <samsiljee@gmail.com>
2026-01-17 12:32:52 +1300

    Working prototype of table to csv and csv to midi (using python package). Still need to work out a way to get text to morse and morse to table format.

--- Morse_code_to_midi.R
+++ Morse_code_to_midi.R
@@ -2,24 +2,48 @@
 # Sam Siljee
 # 16 January 2026
 
+# Note the use of a python function to convert from the csv to midi:
+# https://github.com/timwedde/py_midicsv
+# pip install py_midicsv
+
 # Libraries
 library(dplyr)
 
 # Input
 text_file <- "text.txt"
-Morse_code_file <- "morse.csv"
+morse_code_file <- "morse_code.csv"
 
 # Variables
-dot_len <- 1/16
-dash_len <- 3/16
-intra_char_len <- 1/16
-inter_char_len <- 3/16
-inter_word_len <- 7/16
+dot_len <- 1
+dash_len <- 3
+intra_char_len <- 1
+inter_char_len <- 3
+inter_word_len <- 7
+base_len <- 16
 
-# Read in the text
+# Read in the data
+morse_key <- read.csv(morse_code_file)
 
 # Convert to Morse code
 
-# Convert to midi
+# Convert to table
 
-# Write midi
+# Add table header
+midi_csv <-
+    bind_rows(
+        data.frame(
+            V1 = c(0,1,1),
+            V2 = c(0,0,0),
+            V3 = c("Header", "Start_track", "Tempo"),
+            V4 = as.integer(c(0, NA, 500000)),
+            V5 = c(1, NA, NA),
+            V6 = c(128, NA, NA)
+        ),
+        midi_table
+    )
+
+# Write table
+write.table(midi_csv, file = "midi_csv.csv", row.names = FALSE, quote = FALSE, col.names = FALSE, sep = ",")
+
+# Convert .csv to MIDI using python package
+system("csvmidipy midi_csv.csv output_midi.mid")
