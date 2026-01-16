449cbf955446454920eaa14f41a4d458c1e64bc7
samsiljee <samsiljee@gmail.com>
2026-01-16 21:16:02 +1300

    Rough outline

---
+++ Morse_code_to_midi.R
@@ -0,0 +1,25 @@
+# Script to read in plain text, then export as midi
+# Sam Siljee
+# 16 January 2026
+
+# Libraries
+library(dplyr)
+
+# Input
+text_file <- "text.txt"
+Morse_code_file <- "morse.csv"
+
+# Variables
+dot_len <- 1/16
+dash_len <- 3/16
+intra_char_len <- 1/16
+inter_char_len <- 3/16
+inter_word_len <- 7/16
+
+# Read in the text
+
+# Convert to Morse code
+
+# Convert to midi
+
+# Write midi
