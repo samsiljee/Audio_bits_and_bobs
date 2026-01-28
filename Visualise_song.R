54535189df3ce62a5db3355ba8e3ac7c7dbcbdc4
samsiljee <samsiljee@gmail.com>
2026-01-28 15:56:14 +1300

    Added template to visualise a song

---
+++ Visualise_song.R
@@ -0,0 +1,25 @@
+# Song visualisation
+# Sam Siljee
+# 28th January 2026
+
+# Script to take a song as input, and the produce a series of frames to visualise the spectra of FFT
+
+# Libraries
+
+# Variables
+song_file <- "song.wav"
+video_fps <- 20
+frame_height <- 1080
+frame_width <- 1920
+background_col_l <- "blue"
+background_col_r <- "gray"
+foreground_col_l <- "gray"
+foreground_col_r <- "blue"
+
+# Load the song
+
+# Extract bitrate
+
+# Function to make plot from data range
+
+# Loop through segments
