e1ed31ab1c2912cf258d8e60d095cee3c83246f7
samsiljee <samsiljee@gmail.com>
2026-05-10 21:52:17 +1200

    Fixed

--- Image_spectrogram_encode.R
+++ Image_spectrogram_encode.R
@@ -12,27 +12,39 @@ file_path <- "ULTRASOUND OBSTETRIC ANATOMY - Set 3 - Image 1.png" # Image to con
 sample_rate <- 44100 # Audio sample rate
 duration <- 1 # Duration of the clip in seconds
 freq_range <- c(50, 20000) # Frequencies to use in the audio
+log_freq <- TRUE # Use linear or log frequencies
 
 # Read in the image data
 image_data <- readPNG(file_path)
 
-# Reduce to 2D greyscale, Sum RGB channels
-image_data <- image_data[,,1] + image_data[,,2] + image_data[,,3]
+# Reduce to 2D greyscale, mean RGB channels
+image_data <- (image_data[,,1] + image_data[,,2] + image_data[,,3]) / 3
 
 # Use frequency range and image height to make vector of frequencies
-freq_vector <- seq(freq_range[2], freq_range[1], length.out = nrow(image_data))
+if(log_freq){
+    freq_vector <- exp(seq(log(freq_range[2]), log(freq_range[1]), length.out = nrow(image_data)))
+} else {
+    freq_vector <- seq(freq_range[2], freq_range[1], length.out = nrow(image_data))
+}
 
-# Initialise blank matrix
+# Make time vector
+time_seq <- seq(0, duration, length.out = sample_rate * duration)
+
+# Initialise blank matrix for sine waves
 audio_matrix <- matrix(data = NA, nrow = nrow(image_data), ncol = sample_rate * duration)
 
 # Loop through image rows and frequency vector
 for(i in 1:nrow(image_data)){
     # Vector of pixel intensities
-    pixel_intensities <- image_data[i,]
+    pixel_intensities <- pixel_intensities <- approx(
+        x = seq(0, 1, length.out = ncol(image_data)),
+        y = image_data[i,],
+        xout = seq(0, 1, length.out = sample_rate * duration),
+        method = "constant"
+    )$y
     
     # Sine wave vector at the right frequency
-    time_seq <- seq(0, freq_vector[i] * pi, length.out = sample_rate * duration)
-    sine_vector <- sin(time_seq)
+    sine_vector <- sin(2 * pi * freq_vector[i] * time_seq)
     
     # Multiply by pixel intensities and add to audio matrix
     audio_matrix[i,] <- pixel_intensities * sine_vector
