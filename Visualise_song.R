915c2c184c94cbd9783bace462b06c1852cafc6c
samsiljee <samsiljee@gmail.com>
2026-05-10 21:29:34 +1200

    Formating and some other edits

--- Visualise_song.R
+++ Visualise_song.R
@@ -39,45 +39,47 @@ fft_data <- data.frame()
 
 # Loop through time segments
 progress <- 0
-for(i in 1:n_frames){
-    # Define times for the frame
-    start_point <- round((i - 1) / video_fps * sample_rate) + 1
-    end_point <- round(i / video_fps * sample_rate)
-    signal_length <- end_point - start_point + 1
-    
-    # Filter and split audio signal
-    l_signal <- l_channel[start_point : end_point]
-    r_signal <- r_channel[start_point : end_point]
-    
-    # Get numerical range and add to contrast vector
-    contrast <- c(
-        contrast,
-        (max(l_signal) + max(r_signal)) - (min(l_signal) + min(r_signal))
+for (i in 1:n_frames) {
+  # Define times for the frame
+  start_point <- round((i - 1) / video_fps * sample_rate) + 1
+  end_point <- round(i / video_fps * sample_rate)
+  signal_length <- end_point - start_point + 1
+
+  # Filter and split audio signal
+  l_signal <- l_channel[start_point:end_point]
+  r_signal <- r_channel[start_point:end_point]
+
+  # Get numerical range and add to contrast vector
+  contrast <- c(
+    contrast,
+    (max(l_signal) + max(r_signal)) - (min(l_signal) + min(r_signal))
+  )
+
+  # Run FFT and add to the data.frame
+  fft_data <- bind_rows(
+    fft_data,
+    data.frame(
+      mz = 1:signal_length,
+      intensity = abs(fft(l_signal)),
+      channel = "l",
+      frame = i
     )
-    
-    # Run FFT and add to the data.frame
-    fft_data <- bind_rows(
-        fft_data,
-        data.frame(
-            mz = 1:signal_length,
-            intensity = abs(fft(l_signal)),
-            channel = "l",
-            frame = i)
+  )
+  fft_data <- bind_rows(
+    fft_data,
+    data.frame(
+      mz = 1:signal_length,
+      intensity = abs(fft(r_signal)),
+      channel = "r",
+      frame = i
     )
-    fft_data <- bind_rows(
-        fft_data,
-        data.frame(
-            mz = 1:signal_length,
-            intensity = abs(fft(r_signal)),
-            channel = "r",
-            frame = i)
-    )
-    
-    # Update progress
-    if(progress != round(i*100/n_frames)){
-        progress <- round(i*100/n_frames)
-        print(paste0("Progress: ", progress, "%"))
-    }
+  )
+
+  # Update progress
+  if (progress != round(i * 100 / n_frames)) {
+    progress <- round(i * 100 / n_frames)
+    print(paste0("Progress: ", progress, "%"))
+  }
 }
 
 # Scale contrast vector
@@ -86,21 +88,22 @@ contrast <- contrast / max(contrast)
 # Add a frequency band variable to the fft data
 signal_length <- max(fft_data$mz)
 band <- c(
-    rep("l_r", floor(signal_length / 6)),
-    rep("l_g", floor(signal_length / 6)),
-    rep("l_b", floor(signal_length / 6)),
-    rep("r_r", floor(signal_length / 6)),
-    rep("r_g", floor(signal_length / 6)),
-    rep("r_b", floor(signal_length / 6)),
-    rep("r_b", signal_length %% 6)
-) %>% as.factor
+  rep("l_r", floor(signal_length / 6)),
+  rep("l_g", floor(signal_length / 6)),
+  rep("l_b", floor(signal_length / 6)),
+  rep("r_r", floor(signal_length / 6)),
+  rep("r_g", floor(signal_length / 6)),
+  rep("r_b", floor(signal_length / 6)),
+  rep("r_b", signal_length %% 6)
+) %>% as.factor()
 fft_data$band <- rep(band, n_frames)
 
-# Make vectors to be used for colours
+# Make vectors to be used for colours, and log 2 convert
 col_data <- fft_data %>%
-    group_by(frame, band) %>%
-    summarise(intensity = mean(intensity)) %>%
-    pivot_wider(names_from = band, values_from = intensity)
+  group_by(frame, band) %>%
+  summarise(intensity = mean(intensity)) %>%
+  pivot_wider(names_from = band, values_from = intensity) %>%
+  log2()
 
 # Normalise and convert to 8-bit
 col_l_r <- round(col_data$l_r / max(col_data$l_r) * 255)
@@ -124,37 +127,37 @@ col_r <- paste0("#", col_r_r, col_r_g, col_r_b)
 
 # Main loop through for each frame
 progress <- 0
-for(i in 1:n_frames){
-    # Filter the data
-    spectrum_left <- fft_data %>%
-        filter(channel == "l" & frame == i) %>%
-        select(mz, intensity)
-    spectrum_right <- fft_data %>%
-        filter(channel == "r" & frame == i) %>%
-        select(mz, intensity)
-    
-    # Make the double-plot
-    double_plot(
-        spectrum_left = spectrum_left,
-        spectrum_right = spectrum_right,
-        colour_1 = col_l[i],
-        colour_2 = col_r[i],
-        contrast = contrast[i])
-    
-    # Save the plot
-    ggsave(paste0(output_dir, i, ".png"), width = frame_width, height = frame_height, units = "px")
-    
-    # Update progress
-    if(progress != round(i*100/n_frames)){
-        progress <- round(i*100/n_frames)
-        print(paste0("Progress: ", progress, "%"))
-    }
+for (i in 1:n_frames) {
+  # Filter the data
+  spectrum_left <- fft_data %>%
+    filter(channel == "l" & frame == i) %>%
+    select(mz, intensity)
+  spectrum_right <- fft_data %>%
+    filter(channel == "r" & frame == i) %>%
+    select(mz, intensity)
+
+  # Make the double-plot
+  double_plot(
+    spectrum_left = spectrum_left,
+    spectrum_right = spectrum_right,
+    colour_1 = col_l[i],
+    colour_2 = col_r[i]
+  )
+
+  # Save the plot
+  ggsave(paste0(output_dir, i, ".png"), width = frame_width, height = frame_height, units = "px")
+
+  # Update progress
+  if (progress != round(i * 100 / n_frames)) {
+    progress <- round(i * 100 / n_frames)
+    print(paste0("Progress: ", progress, "%"))
+  }
 }
 
 # Put the plots together in video format
 av_encode_video(
-    paste0(output_dir, 1:n_frames, ".png"),
-    "video.mp4",
-    framerate = video_fps,
-    audio = song_file
+  paste0(output_dir, 1:n_frames, ".png"),
+  "video.mp4",
+  framerate = video_fps,
+  audio = song_file
 )
