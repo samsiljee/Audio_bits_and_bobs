3c710e6815c1243b56c96916d9fa748ddc432466
samsiljee <samsiljee@gmail.com>
2026-06-26 17:15:10 +1200

    Added template for spectrogram masking project

---
+++ Spectrogram_masking.R
@@ -0,0 +1,19 @@
+# Spectral masking
+# Sam Siljee
+# 26th June 2026
+
+# Project to take an audio track, and mask it using a second audio track
+
+# Import data
+
+# Trim longer audio to the same length as the shorter one
+
+# Use FFT to transform to spectrogram
+
+# Take inverse of masking spectrogram
+
+# Multiply input audio by inverse masking spectrogram
+
+# Revert from spectrogram back to waveform
+
+# Export masked audio
